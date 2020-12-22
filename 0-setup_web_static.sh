1ad2daade6cd718f8780719d848503850505c17d
matcls <manueltorresvesga@gmail.com>
2020-12-22 10:42:54 -0500

    Add bash script that sets up a server for the deployment of web_static
    Install Nginx if it not already installed
    Create the folder /data/ if it doesnt already exist
    Create the folder /data/web_static/ if it doesnt already exist
    Create the folder /data/web_static/releases/ if it doesnt already exist
    Create the folder /data/web_static/shared/ if it doesnt already exist
    Create the folder /data/web_static/releases/test/ if it doesnt already exist
    Create a fake HTML file /data/web_static/releases/test/index.html
    Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder.
    If the symbolic link already exists, it should be deleted and recreated every time the script is ran.
    Give ownership of the /data/ folder to the ubuntu user AND group

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+apt update -y
+apt install -y nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    Nginx server test
+  </body>
+</html>" | tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data
