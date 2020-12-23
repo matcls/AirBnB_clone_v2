efe88e4d8bc0997184cc3da5a7e8c7c2f5780538
matcls <manueltorresvesga@gmail.com>
2020-12-22 19:16:27 -0500

    Setup a web servers for the deployment of web_static
    Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#
+# Setup a web servers for the deployment of web_static.
 apt update -y
 apt install -y nginx
 mkdir -p /data/web_static/releases/test/
@@ -9,8 +9,10 @@ echo "<!DOCTYPE html>
   <head>
   </head>
   <body>
-    Nginx server test
+    <p>Nginx server test</p>
   </body>
 </html>" | tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data
+sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
+sudo service nginx restart
