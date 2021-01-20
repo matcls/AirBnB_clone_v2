5b173f347ef384628df14d6008d5f98810c2b396
matcls <manueltorresvesga@gmail.com>
2021-01-20 17:59:01 -0500

    Update FileStorage to return instantiated objects.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -23,9 +23,17 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Return the dictionary __objects."""
-        return self.__objects
+        if cls is not None:
+            if type(cls) == str:
+               cls = eval(cls)
+            cls_dict = {}
+            for k, v in self.__objects.items():
+                if type(v) == cls:
+                    cls_dict[k] = v
+                return cls_dict
+            return self.__objects
 
     def new(self, obj):
         """Set in __objects the obj with key <obj class name>.id."""
