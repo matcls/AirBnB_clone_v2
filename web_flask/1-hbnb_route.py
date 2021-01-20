7e42818b0e60abd3815926a8057a21def36fbd57
matcls <manueltorresvesga@gmail.com>
2021-01-20 14:19:40 -0500

    Fix Route in hbnb

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -19,7 +19,7 @@ def hello():
     return "Hello HBNB!"
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Display “HBNB"""
     return "HBNB"
